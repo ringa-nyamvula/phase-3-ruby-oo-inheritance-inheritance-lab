71b2b5e494b74c17e7f7ceb8c19fa8192bdb1215
ringa-nyamvula <nyamvularinga@gmail.com>
2023-05-22 00:54:32 +0300

    Complete Assignment

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,13 +1,14 @@
 class Teacher < User
   KNOWLEDGE = [
-    "Physics",
-    "Mathematics",
-    "Biology",
-    "History",
-    "English"
+    "Ruby",
+    "Python",
+    "JavaScript",
+    "Java",
+    "C++"
   ].freeze
-
+  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   def teach
-    self.class::KNOWLEDGE.sample
+    KNOWLEDGE.sample
   end
 end
+
